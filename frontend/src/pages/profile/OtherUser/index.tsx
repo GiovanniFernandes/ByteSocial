6c966ce71096c427d4d426b885aa0577b7379a17
tunim73 <antonio.netto973@gmail.com>
2023-04-08 19:29:44 -0300

    ajeitando html/css OtherUser

--- frontend/src/pages/profile/OtherUser/index.tsx
+++ frontend/src/pages/profile/OtherUser/index.tsx
@@ -1,17 +1,30 @@
 import { useEffect, useState } from "react";
 import styles from './OtherUser.module.scss'
-import Post from '../../../components/Post/Post'
-import interactions from 'data/interactions.json'
 import { useContext } from 'react';
 import { AuthContext } from 'contexts/Auth/AuthContexts'
-import {UserPlus} from 'phosphor-react'
-
+import {BsFillPersonPlusFill} from 'react-icons/bs'
 interface Props {
   selectedMenu: number,
   setSelectedMenu: React.Dispatch<React.SetStateAction<number>>
 }
 
-export default function Home(props: Props) {
+const interactions = [
+  {
+    "title": "posts",
+    "count": 20
+  },
+  {
+    "title": "respostas",
+    "count": 50
+  },
+  {
+    "title": "conexões",
+    "count": 50
+  }
+]
+
+
+export default function OtherUser(props: Props) {
 
 
     useEffect(() => {
@@ -20,7 +33,6 @@ export default function Home(props: Props) {
     }, [])
     const auth = useContext(AuthContext); 
     const [username, setUsername] = useState<string | null>('')
-    const [selectedSection, setSelectedSection] = useState<number>(0)
     const getUser = async () => {
       if(auth.user != null)
           setUsername(auth.user.username) 
@@ -29,20 +41,22 @@ export default function Home(props: Props) {
     }
 
   return (
-    <div className={styles.profileID} id='profile'>
+    <div className={styles.profile} id='profile'>
       <h1 className={styles.profile__Title}> Perfil </h1>
       <div className={styles.profile__OtherUser}>
-        <div className={styles.profile__OtherUser__img}>
-         <img src={`https://avatar.uimaterial.com/?setId=0496UVJDTqyd2eCIAa46&name=${username}`} alt="Foto de perfil" className={styles.profile__OtherUser__img} /> 
+        <div className={styles.profile__OtherUser__pic}>
+         <img src={`https://avatar.uimaterial.com/?setId=0496UVJDTqyd2eCIAa46&name=${username}`} alt="Foto de perfil" className={styles.profile__OtherUser__pic} /> 
         </div>
         <div className={styles.profile__OtherUser__infos}>
-        {/* //aqui vai o ícone que tem no figma */}
           <h2 className={styles.profile__OtherUser__name}>{username}</h2>
-          <div className={styles.profile__OtherUser__connect}>
-            <UserPlus className={styles.profile__OtherUser__connect__icon} size={20}  />
+          <button className={styles.profile__OtherUser__connect}>
+            <BsFillPersonPlusFill
+              className={styles.profile__OtherUser__connect__icon}
+              size={20}
+            />
             <h3 className={styles.profile__OtherUser__connect__text}>Conectar-se</h3>
             
-          </div>
+          </button>
           
           <ul className={styles.profile__OtherUser__interactions}>
               {interactions.map((item) => (
@@ -55,12 +69,11 @@ export default function Home(props: Props) {
           </ul>
         </div>
       </div>
-      <div className={styles.profile__sections}>
-        <div
-            className={selectedSection !== 0 ? styles.profile__sections__item : styles.profile__sections__item__selected}
-            onClick={() => setSelectedSection(0)}>
-            <h3>Publicações</h3>
+      <div className={styles.profile__sectionPosts}>    
+        <div className={styles.profile__sectionPosts_title}>
+          <h3>Publicações</h3>
         </div>
+        
         Xuxa
       </div>
     </div>
