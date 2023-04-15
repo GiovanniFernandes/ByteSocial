b55b4e99f93f6d31f8edca6de04c8fb13de1dd79
devAna <caldas.ana9@gmail.com>
2023-04-15 00:50:12 -0300

    Inicio das alterações no socket

--- backend/src/controllers/MessageSocketController.js
+++ backend/src/controllers/MessageSocketController.js
@@ -1,9 +1,10 @@
 const MessageController = require('../controllers/MessageController.js');
 
 class MessageSocketController {
+  //static connectedUsers = new Map();
   constructor(io) {
     this.io = io;
-    this.connectedUsers = [];
+    this.connectedUsers = new Map();
     this.handleConnection();
   }
   
@@ -13,17 +14,36 @@ class MessageSocketController {
       this.handleDisconnect(socket);
 
       socket.on('add-user', ({ userId, username }) => {
+        this.connectedUsers.set(userId, socket.id);
+        console.log("ESSE AQUI " + this.connectedUsers.get(userId));
+        socket.data.userId = userId;
+
+        /*
         !this.connectedUsers.some((user) => user.userId === userId) &&
           this.connectedUsers.push({ userId, socketID: socket.id });
         socket.data.username = username;
-        this.io.emit('get-users', this.connectedUsers);
+        //this.io.emit('get-users', this.connectedUsers);*/
       });
+
+      async function friendshipVerification(senderId, receiverId){
+
+        //const usuarioReq = await Users.findOne({where:{receiverId}});
+
+        //if(!usuarioReq) return res.status(404).json({msg:"Usuário não existe"})
+
+        const buscaUser = await Connections.findOne({where:{user1_id:senderId, user2_id:receiverId}});
+
+        const buscaEmUser = await Connections.findOne({where:{user1_id:receiverId, user2_id:senderId}});
+
+        if(!buscaUser&&!buscaEmUser) throw new Error("Você não tem vínculo algum com esse usuário");
+      }
       
       socket.on('send-message', async ({ senderId, receiverId, message }) => {
         try {
           const savedMessage = await MessageController.enviarMensagem(receiverId, senderId, message);
           //armazena o id do socket do usuário que recebeu a mensagem
-          const receiverSockets = this.connectedUsers.filter((user) => user.userId === receiverId).map((user) => user.socketID);
+          //const receiverSockets = this.connectedUsers.filter((user) => user.userId === receiverId).map((user) => user.socketID);
+          const receiverSockets = this.connectedUsers.get(receiverId);
           console.log(receiverSockets);
           if (receiverSockets) {
             socket.to(receiverSockets).emit('new-message', {
@@ -41,10 +61,14 @@ class MessageSocketController {
 
   handleDisconnect(socket) {
     socket.on('disconnect', () => {
-      console.log(`Socket disconnected: ${socket.id}`);
+      //this.connectedUsers = this.connectedUsers.filter((user) => user.socketID !== socket.id)
+      const userData = this.connectedUsers.get(socket.data.userId)
 
-      // atualiza a lista sem o usuário que se desconectou
-      this.connectedUsers = this.connectedUsers.filter((user) => user.socketID !== socket.id)
+      if (userData){
+        console.log(`Socket disconnected: ${socket.id}`)
+        this.connectedUsers.delete(socket.data.userId)
+      }
+      
     });
   }
 }
