bc289c053b5a6505ff61d275eb8bfb47b4183fe7
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2023-04-29 19:08:07 -0300

    Criação e estilização do componente de solicitações

--- frontend/src/components/RequestConnection/index.tsx
+++ frontend/src/components/RequestConnection/index.tsx
@@ -1,6 +1,5 @@
-import style from './requestConnection.module.scss'
-
-
+import styles from './requestConnection.module.scss'
+import { X, Check } from "@phosphor-icons/react";
 
 
 
@@ -14,18 +13,28 @@ export default function RequestConnection({user_id, email, username}: Props) {
 
 
     return (
-    <h1>
-        {username}
-        </h1>
+    
+    <div className={styles.Request__content}>
+
+        <div className={styles.Request__content__user}>
+            <div className={styles.Request__content__user__img}>
+            <img src={`https://avatar.uimaterial.com/?setId=0496UVJDTqyd2eCIAa46&name=${username}`} alt="Foto de perfil" className={styles.Request__content__user__img} />
+            </div>
+
+            <div className={styles.Request__content__user__userInfo}>
+                <h4 className={styles.Request__content__user__userInfo__name}>{username}</h4>
+                <p className={styles.Request__content__user__userInfo__RequestEmail}>{email}</p>
+            </div>
+
+            <div className={styles.ButtonContent}>
+            <button style={{backgroundColor:"#36ff6e"}}className={styles.ButtonContent__ButtonOption}><Check size={32} /></button>
+            <button style={{backgroundColor:"#F93944"}}className={styles.ButtonContent__ButtonOption}><X size={32}/></button>
+
+            </div>
+        </div> 
+       
+    </div>
+        
     )
 
 }
-
-/*
-
-OOOOOOOOOOOOOOO  -> 
-
-
-
-
-*/
