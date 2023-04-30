84de8add36ab3df232e7781de8ac984d72f28032
tunim73 <antonio.netto973@gmail.com>
2023-04-29 23:46:05 -0300

    alteração hook useApiConnection - perfect

--- frontend/src/hooks/useApiConnection.ts
+++ frontend/src/hooks/useApiConnection.ts
@@ -4,9 +4,9 @@ import { api, settingAxios  } from "services/api";
 
 export const useApiConnection = () => ({
 
-    newRequest: async (id: string) => {
+    newRequest: async (id: number) => {
 
-        if (id === "" || id === null)
+        if (id === null)
             return false
         
         const settingGeneralAxios = settingAxios();
@@ -14,52 +14,14 @@ export const useApiConnection = () => ({
         if (!settingGeneralAxios)
             return false
 
-        const resposta = await api.post(`/request/${id.toString()}`, {
-            content: id
-        }, settingGeneralAxios )
+        const resposta = await api.post(`/request/${id.toString()}`,
+            {}, settingGeneralAxios)
 
         return resposta.data;
     },
 
-    undoRequest: async (id: string) => {
-        return false
-    },
-
-    acceptRequest: async (id: string) => {
-
-        if (id === "" || id === null)
-            return false
-
-        const settingGeneralAxios = settingAxios();
-
-        if (!settingGeneralAxios)
-            return false
-
-        const resposta = await api.post(`/request/accept/${id.toString()}`,
-            settingGeneralAxios)
-
-        return resposta.data;
-    },
-
-    rejectRequest: async (id: string) => {
-
-        if (id === "" || id === null)
-            return false
-        
-        const settingGeneralAxios = settingAxios();
-
-        if (!settingGeneralAxios)
-            return false
-
-            const resposta = await api.delete(`/request/reject/${id.toString()}`,
-            settingGeneralAxios)
-
-        return resposta.data;
-    },
-
-    undoFriend: async (id: string) => {
-
-        if (id === "" || id === null)
+    cancelRequest: async (id: number) => {
+        if (id === null)
             return false
         
         const settingGeneralAxios = settingAxios();
@@ -69,13 +31,44 @@ export const useApiConnection = () => ({
 
             const resposta = await api.delete(`/request/cancel/${id.toString()}`,
             settingGeneralAxios)
-
+            console.log("cancel request: ", resposta)
         return resposta.data;
     },
 
-    showRequest: async (id: string) => {
+    acceptRequest: async (id: number) => {
 
-        if (id === "" || id === null)
+        if (id === null)
+            return false
+
+        const settingGeneralAxios = settingAxios();
+
+        if (!settingGeneralAxios)
+            return false
+        const resposta = await api.post(`/request/accept/${id.toString()}`,{},
+            settingGeneralAxios)
+        
+        console.log("accpet: ", resposta)
+        return resposta.data;
+    },
+
+    rejectRequest: async (id: number) => {
+
+        if (id === null)  return false
+        
+        const settingGeneralAxios = settingAxios();
+
+        if (!settingGeneralAxios)
+            return false
+
+            const resposta = await api.delete(`/request/reject/${id.toString()}`,
+            settingGeneralAxios)
+            console.log("reject: ", resposta)
+        return resposta.data;
+    },
+    
+    showRequest: async (id: number) => {
+
+        if (id === null)
             return false
         
         const settingGeneralAxios = settingAxios();
