158753dd76046cdd3789cbd6d867f2b0274b5bcf
tunim73 <antonio.netto973@gmail.com>
2023-05-02 00:27:27 -0300

    Menu Context - ok

--- frontend/src/contexts/Menu/MenuContexts.tsx
+++ frontend/src/contexts/Menu/MenuContexts.tsx
@@ -1,43 +1,13 @@
 import { createContext, useContext } from "react";
 
-
-export type MenuContextType = {
+export interface MenuContextType  {
     selectedItemMenu: number,
     setSelectedItemMenu: React.Dispatch<React.SetStateAction<number>>,
     openState: boolean,
     setOpenState: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+export const MenuContext = createContext<MenuContextType>(null!); 
 
-export const _MenuContext = createContext<MenuContextType>(null!); 
+export const useMenuContext = () => useContext(MenuContext);
 
-
-
-
-
-/*
-Formas de encerrar os props inuteis ! 
-
-Quando eu entrar na página, vai ter o useEfect que vai setar para onde eu quero
-
-Quem pega a informação deve ser um context,
-logo o state e o setState devem ser globais, estando no context
-
-o context pega tal informação e dispoê pro menu
-
-OR
-
---------------------------------------------------
-
-Referente a questão de abrir e fechar, percebo que o estado de menu aberto e menu fechado também precisará ser colocado globalmente.
-
-Assim, ao iniciar o arquivo eu pego o estado do menu, se é aberto ou fechado e faço isso para controlar a margin que será necessário colocar, talvez, tal margin seja preferivel deixar no context
-
-por que caso eu precise mudar, eu apenas lá
---------------------------------------------------
-
-1° passo fazer o contexto, de acordo com o que foi dito acima
-2° passo fazer funcionar como já sendo
-3° aplicar o abrir e fechar
-
-*/
--- frontend/src/contexts/Menu/MenuProvider.tsx
+++ frontend/src/contexts/Menu/MenuProvider.tsx
@@ -1,6 +1,5 @@
-import { useContext, useState } from "react"
-import { _MenuContext } from "./MenuContexts"
-
+import { useState } from "react"
+import { MenuContext } from "./MenuContexts"
 
 
 export const MenuProvider = ( {children} : {children:JSX.Element} )  => {
@@ -8,15 +7,13 @@ export const MenuProvider = ( {children} : {children:JSX.Element} )  => {
     const [selectedItemMenu, setSelectedItemMenu] = useState<number>(1)
     const [openState, setOpenState] = useState<boolean>(true)
     return(
-        <_MenuContext.Provider value={{
+        <MenuContext.Provider value={{
             selectedItemMenu,
             setSelectedItemMenu,
             openState,
             setOpenState
         }}>
             {children}
-        </_MenuContext.Provider>
+        </MenuContext.Provider>
     )
 }
-
-//export const menuContext = useContext(_MenuContext);
